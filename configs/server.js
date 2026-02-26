6e28d4b6065b9b5a154f32f3b14b3449ecd51b53
asiliezar-2024342 <asiliezar-2024342@kinal.edu.gt>
2026-02-26 01:17:04 -0600

    feat: Implementar seeder de cuenta para el banco por defecto

---
+++ configs/server.js
@@ -0,0 +1,29 @@
+import Account from '../src/users/user.model.js';
+// Seeder para crear por defecti una cuenta para el banco
+
+export const seedBankAccount = async () => {
+    try {
+        const existingAccount = await Account.findOne({ accountNumber: '0000000000' });
+
+        if (existingAccount) {
+            console.log('Cuenta del banco ya existe... Saltando seeder.');
+            return;
+        }
+        const bankAccount = new Account({
+            authUserId: 'BANK_ACCOUNT',
+            accountNumber: '0000000000',
+            dpi: '0000000000000',
+            address: 'Fundación Kinal, Ciudad de Guatemala',
+            phone: '00000000',
+            jobName: 'Sistema Bancario',
+            monthlyIncome: 999999999,
+            balance: 999999999,
+            isActive: true
+        });
+
+        await bankAccount.save();
+        console.log('Cuenta del banco creada exitosamente');
+    } catch (error) {
+        console.error('Error al crear la cuenta del banco:', error);
+    }
+};
